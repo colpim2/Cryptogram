feaf38a44daf1f4414b50c56a4c93a7f0bafaf83
colpim2 <pamela.castle@hotmail.com>
2024-04-12 21:47:17 -0600

    Flask template

---
+++ main.py
@@ -0,0 +1,59 @@
+# pip install flask
+# pip install flask-socketio
+
+from flask import Flask, render_template, request, session, redirect, url_for
+from flask_socketio import send,SocketIO
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret!"
+socketio = SocketIO(app)
+
+@app.route("/", methods=["POST","GET"])   #Post & Get data
+def home():
+  session.clear()
+  # If there is a request (insert password)
+  if request.method == "POST":
+    name = request.form.get("name")
+    password = request.form.get("password")
+    join = request.form.get("join")
+
+    if not name:
+      return render_template("home.html", error="Please enter a name.", password=password, name=name)
+
+    if join != False and not password:
+      return render_template("home.html", error="Please enter a password", password=password, name=name)
+
+    return redirect(url_for("chat"))
+
+  return render_template("home.html")
+
+@app.route("/chat")
+def chat():
+  password = session.get("password")
+  return render_template("chat.html")
+
+@socketio.on("connect")
+def connect(auth):
+    name = session.get("name")
+    print(f"{name} joined")
+
+@socketio.on("disconnect")
+def disconnect():
+  name = session.get("name")
+  print(f"{name} has left")
+
+@socketio.on("message")
+def message(data):
+    content = {
+        "name": session.get("name"),
+        "message": data["data"]
+    }
+    print(f"{session.get('name')} said: {data['data']}")
+
+# Initialize app server
+if __name__ == "__main__":
+  socketio.run(app,debug=True)  #True: Automatic refresh
+
+
+
+
